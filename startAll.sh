6e699d60525181121e947f9bd1ad21702632d51e
Wutthiphat Phuriphatwatthana <Ken.Wutthiphat@Live.com>
2016-08-20 00:42:23 +0700

    Fix Start Script to Run as User

--- startAll.sh
+++ startAll.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-screen -S cmsAdminWebServer -d -m /bin/bash -c 'sudo /usr/local/bin/cmsAdminWebServer'
-screen -S cmsResourceService -d -m /bin/bash -c 'sudo /usr/local/bin/cmsResourceService -a 1'
-screen -S cmsLogService -d -m /bin/bash -c 'sudo /usr/local/bin/cmsLogService'
+screen -S cmsAdminWebServer -d -m /bin/bash -c '/usr/local/bin/cmsAdminWebServer'
+screen -S cmsResourceService -d -m /bin/bash -c '/usr/local/bin/cmsResourceService -a 1'
+screen -S cmsLogService -d -m /bin/bash -c '/usr/local/bin/cmsLogService'
