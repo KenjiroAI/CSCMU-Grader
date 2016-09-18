efb8944d5ad642a73e410823736b60898b503d34
Wutthiphat Phuriphatwatthana <Ken.Wutthiphat@Live.com>
2016-09-19 01:12:50 +0700

    Fix startup script for crontab

--- startAll.sh
+++ startAll.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-screen -S cmsAdminWebServer -d -m /bin/bash -c '/usr/local/bin/cmsAdminWebServer'
-screen -S cmsResourceService -d -m /bin/bash -c '/usr/local/bin/cmsResourceService -a 1'
-screen -S cmsLogService -d -m /bin/bash -c '/usr/local/bin/cmsLogService'
+
+# Add CMS Location to Environment (for crontab)
+PATH=$PATH:/usr/local/bin
+export PATH
+
+# Run CMS
+/usr/bin/screen -S cmsResourceService -d -m /bin/bash -c '/usr/local/bin/cmsResourceService -a 1'
+/usr/bin/screen -S cmsLogService -d -m /bin/bash -c '/usr/local/bin/cmsLogService'
